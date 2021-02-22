142faca36f2f1648b59d586a36dd1163283076d6
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:21:11 +0100

    Create wag.py

---
+++ gifs/wag.py
@@ -0,0 +1,6 @@
+wag = ["https://media.tenor.com/images/391f32ea35c6bd010fe29425f8a689e3/tenor.gif",
+       "https://media.tenor.com/images/bac4a23e4a961a274c94d2631cf63e19/tenor.gif",
+       "https://media1.tenor.com/images/3562622d1c072ba2c30645b19e13bd2f/tenor.gif?itemid=7910058",
+       "https://media.tenor.com/images/4a53db6c0267f84b180d7b9d3c34f120/tenor.gif",
+       "https://media.tenor.com/images/dd137df1f2badd31405967a12d862f28/tenor.gif",
+       "https://media.tenor.com/images/7bd999202fde3cb64395725964ff34ac/tenor.gif"]
