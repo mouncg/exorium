4a052666a13d12adcef250063519a11cb6fb91d6
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:12:26 +0100

    Create awoo.py

---
+++ gifs/awoo.py
@@ -0,0 +1,8 @@
+awoo = ["https://cdn.discordapp.com/attachments/715969701771083820/739429902402715748/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430343962001468/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430488091000863/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430548895825930/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430608673177650/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430699672797324/unknown.gif",
+        "https://media.giphy.com/media/jPSjVZLPOGVa0/giphy.gif", "https://media.giphy.com/media/lLoYlICw9DEY0/giphy.gif",
+        "https://media.giphy.com/media/OQI1KMVGJyniwVwnl3/giphy.gif", "https://media.giphy.com/media/2nXYb1jf0FVRK/giphy.gif"]
