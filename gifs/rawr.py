49707f728f29aabf95d99494a57c784ad029d739
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:31:50 +0100

    Delete rawr.py

--- gifs/rawr.py
+++
@@ -1,5 +0,0 @@
-rawr = ["https://media1.tenor.com/images/5393afcde44d4be4569108e40fddb3a1/tenor.gif?itemid=15945972",
-        "https://media1.tenor.com/images/0ffb715776443e6242b736420ce84144/tenor.gif?itemid=14558168",
-        "https://media1.tenor.com/images/6f5c4dd655d8d93b5f85d2f739f11cea/tenor.gif?itemid=16986104",
-        "https://media1.tenor.com/images/78e79fa52e3934f693ce670a3a905e64/tenor.gif?itemid=16549140"
-        "https://media1.tenor.com/images/84b80c7e1fa843fd4ea7b90e1bc269c4/tenor.gif?itemid=15426315"]
