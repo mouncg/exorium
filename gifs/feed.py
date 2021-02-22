19cb1ea36e03d7245d913c8141f2c55f8772724f
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:13:09 +0100

    Create feed.py

---
+++ gifs/feed.py
@@ -0,0 +1,5 @@
+feed = ["https://media1.tenor.com/images/5dda2ba3a73de6e8a3abfc45996ed1ba/tenor.gif?itemid=5879575",
+        "https://media1.tenor.com/images/9a861ec0dcdae5d505afbeca28910182/tenor.gif?itemid=6101207",
+        "https://media1.tenor.com/images/d598225fd7e2ba8bd82793856c78b19f/tenor.gif?itemid=4909807",
+        "https://media1.tenor.com/images/9337abd3a4247c73c218ea3c3662137d/tenor.gif?itemid=7266798",
+        "https://media.tenor.com/images/49109825019cb719d771ff54876fe078/tenor.gif"]
