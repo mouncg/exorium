e106d1036b52e30739d260363ce8c3e42d6a61a7
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:01:55 +0100

    Create hug.py

---
+++ gifs/hug.py
@@ -0,0 +1,9 @@
+hug = ["https://media1.tenor.com/images/faf29ddc23059c5740751a3fecc2f303/tenor.gif?itemid=13533690",
+       "https://media1.tenor.com/images/f20151a1f7e003426ca7f406b6f76c82/tenor.gif?itemid=13985247",
+       "https://media1.tenor.com/images/8ac5ada8524d767b77d3d54239773e48/tenor.gif?itemid=16334628",
+       "https://media1.tenor.com/images/68f16d787c2dfbf23a4783d4d048c78f/tenor.gif?itemid=9512793",
+       "https://media1.tenor.com/images/3fee00811a33590e4ee490942f233c78/tenor.gif?itemid=14712845",
+       "https://media1.tenor.com/images/2d13ede25b31d946284eaa3b8a4e6b31/tenor.gif?itemid=11990658",
+       "https://media1.tenor.com/images/37df0fae36f9cce061c3cec84fc97a08/tenor.gif?itemid=17781844",
+       "https://media1.tenor.com/images/ece75ca15b715aacd86724ee23604569/tenor.gif?itemid=16796068",
+       "https://media2.giphy.com/media/gl8ymnpv4Sqha/giphy.gif"]
