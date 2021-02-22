4a60ce6fb8c9e43f4a2a9523eb51ae2692fea553
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:05:15 +0100

    Create boop.py

---
+++ gifs/boop.py
@@ -0,0 +1,8 @@
+boop = ["https://media1.tenor.com/images/36d187af3d5e876ec0d8f11cabe1a8d4/tenor.gif?itemid=15225693",
+        "https://media1.tenor.com/images/ea37d5c281b7ca7c35175bdc854b0a5b/tenor.gif?itemid=11159084",
+        "https://media.tenor.com/images/3f364e971453b18a1ae2b5cc8d2d0613/tenor.gif",
+        "https://media1.tenor.com/images/a0f0c6b3ef95bb2ff530a92d6c516cbd/tenor.gif?itemid=14452125",
+        "https://media1.tenor.com/images/5c0b2e9661b5ea40834fc6bd6b84a806/tenor.gif?itemid=14796547",
+        "https://cdn.discordapp.com/attachments/715979499799445577/717050110667784272/8NgFDIL.gif",
+        "https://media.tenor.com/images/aa41ce7ea8f138eb8e0be635cd4664d9/tenor.gif",
+        "https://media1.tenor.com/images/9e42f5059137a153137af77cb898c070/tenor.gif?itemid=17599907"]
