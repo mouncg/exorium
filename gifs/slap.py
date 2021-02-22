10f38d0e7da299f1c3d687a7d3c1628e570ffec7
Bluewy <bluewyechache@gmail.com>
2021-02-22 15:52:11 +0100

    Create slap.py

---
+++ gifs/slap.py
@@ -0,0 +1,4 @@
+slap = ["https://media1.tenor.com/images/1ed93f40b89cab10e8f5328f180f1241/tenor.gif?itemid=16415016",
+        "https://media1.tenor.com/images/37bc5bf5e95aa6afcd66fd779b923af5/tenor.gif?itemid=9629173",
+        "https://media1.tenor.com/images/13826f5f3060ce41d8fbe4d16a7acb63/tenor.gif?itemid=5428402",
+        "https://media1.tenor.com/images/415b6bd8c43eb6e8bf660f86b7b98581/tenor.gif?itemid=5730795"]
