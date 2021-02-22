6658bdea5bbe3f439aee9abe0be0b2033b14b5db
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:31:27 +0100

    Delete lick.py

--- gifs/lick.py
+++
@@ -1,6 +0,0 @@
-lick = ["https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif",
-        "https://media1.tenor.com/images/1b96b02b98756a1a45fdde87dacbb36b/tenor.gif?itemid=17522184",
-        "https://media.giphy.com/media/8v9njaWnlcW2c/giphy.gif",
-        "https://media1.tenor.com/images/f40e1b6271581699aed3c12d042c129c/tenor.gif?itemid=15743713",
-        "https://media1.tenor.com/images/cd4f8199d49badf7b90277f8b13f5186/tenor.gif?itemid=12816950",
-        "https://media1.tenor.com/images/cd45350c528cc577cbe0eb754e6e8241/tenor.gif?itemid=12181805"]
