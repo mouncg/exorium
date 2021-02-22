71c4eaaf403e53b5b272c40806108ef998e128a9
Bluewy <bluewyechache@gmail.com>
2021-02-22 23:00:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,29 +26,6 @@ bot = commands.Bot(
     description="A very gay and annoying beta bot"
 )
 
-class EditingContext(commands.Context):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    async def send(self, content=None, *, tts=False, embed=None, file=None, files=None, delete_after=None, nonce=None, allowed_mentions=discord.AllowedMentions.none()):
-        if file or files:
-            return await super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce, allowed_mentions=allowed_mentions)
-        reply = None
-        try:
-            reply = self.bot.cmd_edits[self.message.id]
-        except KeyError:
-            pass
-        if reply:
-            try:
-                msg = await reply.edit(content=content, embed=embed, delete_after=delete_after, allowed_mentions=allowed_mentions)
-                return msg
-            except:
-                return
-        msg = await super().send(content=content, tts=tts, embed=embed, file=file, files=files, delete_after=delete_after, nonce=nonce, allowed_mentions=allowed_mentions)
-        self.bot.cmd_edits[self.message.id] = msg
-        return msg
-
-
 @commands.Cog.listener()
 async def on_ready():
     #activity = discord.Activity(type=discord.ActivityType.watching, name="a movie")
