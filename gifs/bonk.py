a03b5ab57dfe2ab971db19a01697608a9d9eca5b
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:29:17 +0100

    Delete bonk.py

--- gifs/bonk.py
+++
@@ -1,4 +0,0 @@
-bonk = ["https://media1.tenor.com/images/c7db074a1f169d09f037e4a1695ce8ab/tenor.gif?itemid=18746295",
-        "https://media1.tenor.com/images/c658ef8183c978dd6fb528d4c06632c2/tenor.gif?itemid=17177822",
-        "https://media1.tenor.com/images/c70e5c67f32b126e8d62bb1ee099aec4/tenor.gif?itemid=17872492",
-        "https://media1.tenor.com/images/5be9d9780f7f36146eef8ec6716493a5/tenor.gif?itemid=17119858"]
