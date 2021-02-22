5c49c6519f4242315d3c862e2598f9f9360a990b
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:33:03 +0100

    Update functions.py

--- outsources/functions.py
+++ outsources/functions.py
@@ -2,8 +2,6 @@ import discord
 import config
 import random
 import gifs
-from gifs import snuggle
-from gifs import slap
 
 async def interactions(ctx, members, reason, type, ending, typespecial):
     GIFlist = getattr(gifs, type)
