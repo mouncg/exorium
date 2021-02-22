6c8fd39ae360548c6241c395c3894ecc94e909a2
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:43:09 +0100

    Update functions.py

--- outsources/functions.py
+++ outsources/functions.py
@@ -8,7 +8,7 @@ async def interactions(ctx, members, reason, type, ending, typespecial):
     GIF = random.choice(GIFlist)
     if not (members):
         return await ctx.send(f"Please specify at least one cutie to {type}!")
-    embed = discord.Embed(title="", color=config.color, description=(ctx.message.author.mention + " " + f"**{typespecial}**" + " " + '**,** '.join(x.mention for x in members) + f"**, {ending}!**\nFor: " + reason))
+    embed = discord.Embed(title="", color=config.color, description=(ctx.author.mention + " " + f"**{typespecial}**" + " " + '**,** '.join(x.mention for x in members) + f"**, {ending}!**\nFor: " + reason))
     embed.set_footer(text="Check info about the gifs/images with exo help social, and help us!")
     embed.set_image(url=GIF)
     await ctx.send(embed=embed)
