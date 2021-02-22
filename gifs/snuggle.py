62d6b8e14a0402842adb74064bc67e221d7c6cb7
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:32:29 +0100

    Delete snuggle.py

--- gifs/snuggle.py
+++
@@ -1,3 +0,0 @@
-snuggle = ["https://media1.tenor.com/images/ba7829e33a9392d38e15cdc4c650b897/tenor.gif?itemid=17325139",
-           "https://media.giphy.com/media/kz01wlAoCLYx3v8F68/giphy.gif",
-           "https://media.giphy.com/media/DAX8RixOTrWuY/giphy.gif"]
