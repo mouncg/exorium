872bcd30d4e807331c976ed1772ba9f0dac2757b
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:03:13 +0100

    Create pat.py

---
+++ gifs/pat.py
@@ -0,0 +1,5 @@
+pat = ["https://media1.tenor.com/images/1786de6797fd096ff503d633254786cc/tenor.gif?itemid=14598566",
+       "https://media1.tenor.com/images/be0c22e0af951aa7fa8753381663eb2c/tenor.gif?itemid=15824856",
+       "https://media1.tenor.com/images/fd7365b518ee53285f39ef8b1caf5b6e/tenor.gif?itemid=15523978",
+       "https://media1.tenor.com/images/adeb267fbe4ab5ddaa080297e5b2d0e4/tenor.gif?itemid=15740749",
+       "https://media1.tenor.com/images/62d861f1f0b6277e2056e86f4ba42e6f/tenor.gif?itemid=16276220"]
