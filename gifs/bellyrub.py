007fc17aa35bba7cbc3119436d4e27878f1abfef
Bluewy <bluewyechache@gmail.com>
2021-02-22 21:28:37 +0100

    Delete bellyrub.py

--- gifs/bellyrub.py
+++
@@ -1,7 +0,0 @@
-bellyrub = ["https://i.imgur.com/VnoVdts.gif",
-            "https://i.imgur.com/AGb1AKb.gif",
-            "https://i.imgur.com/PbYykOz.gif",
-            "https://i.imgur.com/xxx0Ykb.gif",
-            "https://media1.tenor.com/images/904ca94bb9335013cf8e1d57115de5c5/tenor.gif?itemid=13642623",
-            "https://media1.tenor.com/images/d13c4bcb8939ab000deab59bb46ec189/tenor.gif?itemid=18000915",
-            "https://media1.tenor.com/images/93b76d5e74f7b39dc4404aaa2a5c6fa4/tenor.gif?itemid=7325356"]
