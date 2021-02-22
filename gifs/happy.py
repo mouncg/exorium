3e513de84e04b87c459fd117921b1583af485479
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:13:47 +0100

    Create happy.py

---
+++ gifs/happy.py
@@ -0,0 +1,11 @@
+happy = ["https://media1.tenor.com/images/4b5d8967536d7235f5231ba9123964ab/tenor.gif?itemid=8675372",
+         "https://media.tenor.com/images/6a4617fb4777558efe818e2fa241c5fb/tenor.gif",
+         "https://media.tenor.com/images/7559368ede131c2f119cd19a4a6f7cf5/tenor.gif",
+         "https://media.tenor.com/images/f7ba3433bb89a484ec92d33b87f54ca7/tenor.gif",
+         "https://media.tenor.com/images/8784dfd64f06f19e1a25cb88f990b2b7/tenor.gif",
+         "https://media1.tenor.com/images/aca1a8cd3d8d7bf7d5703bf3f8117a4b/tenor.gif?itemid=5567628",
+         "https://media1.tenor.com/images/ab69ea03faa81a44af51c86770745f61/tenor.gif?itemid=9273180",
+         "https://media1.tenor.com/images/685f1055a8a4dbfe3318e5898d19b97c/tenor.gif?itemid=10253595",
+         "https://media1.tenor.com/images/41df672a1055e4f833e2022dce9231fd/tenor.gif?itemid=16660700",
+         "https://d.facdn.net/art/bluemachine/1573314972/1573314972.bluemachine_2.gif",
+         "https://media.tenor.com/images/a6fc8a84da7b76176d5b79d56c90ffb3/tenor.gif"]
