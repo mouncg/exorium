c17f6f98136e8eb392db49e6b04d8e08164678e5
Bluewy <bluewyechache@gmail.com>
2021-02-22 16:14:08 +0100

    Create highfive.py

---
+++ gifs/highfive.py
@@ -0,0 +1,8 @@
+highfive = ["https://media1.tenor.com/images/bbc34d4c1409ddf777186c773334e3e6/tenor.gif?itemid=6123039",
+            "https://media.tenor.com/images/975d05f75a11fca62e2bb0b1ee040772/tenor.gif",
+            "https://media.tenor.com/images/be4e523990540bb9ad18ac49439e5cd2/tenor.gif",
+            "https://media.tenor.com/images/56eaf6892f8dd40c5f2b4580e8cd34e8/tenor.gif",
+            "https://media1.tenor.com/images/88ece6b5cf8b9df04c6290c5100a8410/tenor.gif?itemid=7367090",
+            "https://media1.tenor.com/images/ce04ff64e42ac6a0fc23dc1e1da79a2c/tenor.gif?itemid=14400946",
+            "https://media1.tenor.com/images/6e5ab91c3133061f67a1843df9ace592/tenor.gif?itemid=5442827",
+            "https://media1.tenor.com/images/59e5bbd7e22503afa1b0ec8176acaae9/tenor.gif?itemid=3431650"]
