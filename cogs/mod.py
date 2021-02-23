588810a2ad05f75525c677b753265845cdb09122
Joshua <bluewyechache@gmail.com>
2021-02-23 16:25:28 +0100

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -34,7 +34,7 @@ class mod(commands.Cog, name="Moderation"):
         try:
             await ctx.message.delete()
             await ctx.guild.unban(user.user, reason=f"moderator: {ctx.message.author} | {reason}")
-            await ctx.send(f"**{user.user}** was unbanned successfully, with reason: ``{reason}``", delete_after=15)
+            await ctx.send(f"**{user}** was unbanned successfully, with reason: ``{reason}``", delete_after=15)
         except Exception as e:
             await ctx.send(f"```py\n{e}\n```")
 
@@ -42,19 +42,19 @@ class mod(commands.Cog, name="Moderation"):
     @commands.guild_only()
     @commands.has_permissions(ban_members=True)
     @commands.bot_has_permissions(ban_members=True, manage_messages=True)
-    async def softban(self, ctx, user: discord.Member, *, reason="No reason provided"):
+    async def softban(self, ctx, member: discord.Member, *, reason="No reason provided"):
         try:
             if user == ctx.message.author:
                 return await ctx.send("You can not softban yourself, please try someone else.")
             botmember = ctx.guild.me
-            if not botmember.top_role > user.top_role:
+            if not botmember.top_role > member.top_role:
                 return await ctx.send("My role is too low in the hierarchy. Please move it above the highest role the user you are trying to ban has.")
             await ctx.message.delete()
             messageok = f"You were softbanned from `{ctx.guild.name}` with reason:\n\n{reason}"
-            await user.send(messageok)
-            await user.ban(reason=f"Moderator {ctx.message.author} | Reason: {reason}")
-            await ctx.guild.unban(user.user, reason=f"modertor: {ctx.message.author} | softban")
-            e = discord.Embed(title=f"{user} was banned | {reason}", color=config.red)
+            await member.send(messageok)
+            await member.ban(reason=f"Moderator {ctx.message.author} | Reason: {reason}")
+            await ctx.guild.unban(member, reason=f"moderator: {ctx.message.author} | softban")
+            e = discord.Embed(title=f"{member} was banned | {reason}", color=config.red)
             await ctx.send(embed=e)
         except Exception as e:
             await ctx.send(f"```py\n{e}\n```")
