96fea449e4286c5590609b950ae98057d0ac4e73
Joshua <bluewyechache@gmail.com>
2021-02-23 12:42:37 +0100

    Create mod.py

---
+++ cogs/mod.py
@@ -0,0 +1,16 @@
+import discord, config
+from discord.ext import commands
+
+class mod(commands.Cog, name="Moderation"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief="Ban someone")
+    async def ban(self, ctx, member: discord.Member, *, reason="No reason provided"):
+        try:
+            if member == ctx.message.author:
+                return await ctx.send('You can not ban yourself, please try someone else.")
+        await ctx.send('Just a minor test')
+
+def setup(bot):
+    bot.add_cog(mod(bot))
