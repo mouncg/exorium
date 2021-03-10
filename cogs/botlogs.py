eebc38027783e5e7bf0f70418da82ac8e50cc736
Joshua <bluewyechache@gmail.com>
2021-03-10 15:13:48 +0100

    Update botlogs.py

--- cogs/botlogs.py
+++ cogs/botlogs.py
@@ -15,14 +15,15 @@ class botlogs(commands.Cog, name="Bot logs"):
         e = discord.Embed(color=config.color)
         e.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         e.description = f"""
-Message content: 
+**Message content:**
 {ctx.message.clean_content}
 
-Author ID: [{ctx.author.id}](https://discord.com/users/{ctx.author.id})
+**Author ID:** [{ctx.author.id}](https://discord.com/users/{ctx.author.id})
 
-Guild: {ctx.guild.name} (`{ctx.guild.id}`) 
+**Guild:** {ctx.guild.name} (`{ctx.guild.id}`) 
 """
-        #e.timestamp = datetime.now().__format__('%a %d %b %y, %H:%M')
+        timestamp = datetime.now().__format__('%a %d %b %y, %H:%M')
+        e.timestamp=timestamp
         await log.send(embed=e)
 
 def setup(bot):
