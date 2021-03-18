d5dad26bd758c2b8c02cf98cab9b36074f57b264
Joshua <bluewyechache@gmail.com>
2021-03-18 20:09:48 +0100

    Comment out channel command logging

--- cogs/botlogs.py
+++ cogs/botlogs.py
@@ -9,22 +9,22 @@ class botlogs(commands.Cog, name="Bot logs"):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command(self, ctx):
-        log = self.bot.get_channel(755138117488345118)
+    #async def on_command(self, ctx):
+    #    log = self.bot.get_channel(755138117488345118)
         
-        e = discord.Embed(color=config.color)
-        e.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        e.description = f"""
-**Message content:**
-{ctx.message.content}
+    #    e = discord.Embed(color=config.color)
+    #    e.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+    #    e.description = f"""
+#**Message content:**
+#{ctx.message.content}
 
-**Author ID:** [{ctx.author.id}](https://discord.com/users/{ctx.author.id})
+#**Author ID:** [{ctx.author.id}](https://discord.com/users/{ctx.author.id})
 
-**Guild:** {ctx.guild.name} (`{ctx.guild.id}`) 
-"""
-        e.set_footer(text=datetime.now().__format__('%a %d %b %y, %H:%M'))
-        e.set_thumbnail(url=ctx.guild.icon_url)
-        await log.send(embed=e)
+#**Guild:** {ctx.guild.name} (`{ctx.guild.id}`) 
+#"""
+#        e.set_footer(text=datetime.now().__format__('%a %d %b %y, %H:%M'))
+#        e.set_thumbnail(url=ctx.guild.icon_url)
+#        await log.send(embed=e)
         
         print(f"{datetime.now().__format__('%a %d %b %y, %H:%M')} - {ctx.guild.name} | {ctx.author} > {ctx.message.clean_content}") 
 
