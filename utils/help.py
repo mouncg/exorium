2f306a3be06121f749255764476381bc5a462975
Joshua <bluewyechache@gmail.com>
2021-03-30 12:02:24 +0200

    Update help.py

--- utils/help.py
+++ utils/help.py
@@ -89,7 +89,8 @@ class PenguinHelp(commands.HelpCommand):
         embed = discord.Embed(title= f"[{command.cog.qualified_name}] {command.qualified_name}", color=config.color)
             #title= command.qualified_name + " | " + " | ".join([f"{alias}" for alias in command.aliases]),
         embed.description = command.help or f"`{command.qualified_name}` does not have a description."
-
+        embed.thumbnail(url=self.bot.user.avatar_url)
+        
         command = (await self.filter_commands([command]))
 
         command = command[0] if len(command) == 1 else None
