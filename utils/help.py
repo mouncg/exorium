35ed9a8166b1c2146ecf806585a1f95f08aea271
Joshua <bluewyechache@gmail.com>
2021-03-30 11:42:57 +0200

    Update help.py

--- utils/help.py
+++ utils/help.py
@@ -83,7 +83,12 @@ class PenguinHelp(commands.HelpCommand):
         await menu.start(self.context)
 
     async def send_command_help(self, command):
-        embed = discord.Embed(title= f"[{command.cog.qualified_name}] {command.qualified_name}" + " " + ", ".join([f"{alias}" for alias in command.aliases]),
+        
+        aliases = '`' + '`, `'.join(command.aliases) + "`"
+        if aliases == "``" or aliases == '`':
+            aliases = "No aliases were found"
+
+        embed = discord.Embed(title= f"[{command.cog.qualified_name}] {command.qualified_name} {aliases}"),
             #title= command.qualified_name + " | " + " | ".join([f"{alias}" for alias in command.aliases]),
                               description = command.help or f"`{command.qualified_name}` does not have a description.",
                               color=config.color)
