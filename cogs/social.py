e84866b3185908016f810b29f698f3dc0238d850
Joshua <bluewyechache@gmail.com>
2021-03-30 13:38:34 +0200

    Update social.py

--- cogs/social.py
+++ cogs/social.py
@@ -154,7 +154,7 @@ class social(commands.Cog, name="Social"):
                 
                 if not members:
                     return await ctx.send("Please specify someone to hug.")
-                e = discord.Embed(color=config.color, description=f"**{ctx.message.author.mention} hugs** " + '**,** '.join(x.mention for x in members) + {reason})
+                e = discord.Embed(color=config.color, description=f"**{ctx.message.author.mention} hugs** " + '**,** '.join(x.mention for x in members) + "For: {reason}")
                 
                 manual = gifs.hug
                 manual.append(js['link'])
