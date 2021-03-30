3d184a90dc4141a96410e1824f45adb98a17ac78
Joshua <bluewyechache@gmail.com>
2021-03-30 13:01:44 +0200

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -8,11 +8,13 @@ class utility(commands.Cog, name="Utility"):
 
     @commands.command(brief="Get someone's ID")
     async def id(self, ctx, member: discord.Member):
+        """Get a server member's ID"""
         await ctx.send(member.id)
 
 
-    @commands.command(brief="See someone's av", aliases=["av"])
+    @commands.command(brief="See someone's avatar", aliases=["av"])
     async def avatar(self, ctx, *, user: discord.Member = None):
+        """Shows the avatar of the specified user"""
         if not user:
             user = ctx.author
         e = discord.Embed(color=config.color)
@@ -23,6 +25,7 @@ class utility(commands.Cog, name="Utility"):
     
     @commands.command(brief="See server icon")
     async def servericon(self, ctx):
+        """Shows the icon of the server you are in"""
         e = discord.Embed(color=config.color)
         e.set_author(name=f"Icon of {ctx.guild.name}", icon_url=ctx.guild.icon_url)
         e.set_image(url=ctx.guild.icon_url)
@@ -31,12 +34,14 @@ class utility(commands.Cog, name="Utility"):
 
     @commands.command(brief="Random selection")
     async def random(self, ctx, *args):
+        """Select a random option out of the given options"""
         if not args:
             return await ctx.send("Please give more than 1 argument for me to choose from.")
         await ctx.send(f"I choose `{random.choice(args)}`.")
 
     @commands.command(brief="Host a poll")
     async def poll(self, ctx, *, args):
+        """Make a yes/no poll"""
         try:
             await ctx.message.delete()
         except discord.Forbidden:
@@ -50,6 +55,7 @@ class utility(commands.Cog, name="Utility"):
 
     @commands.command(brief="Say something")
     async def say(self, ctx, *, args):
+        """Say something through the bot"""
         try:
             await ctx.message.delete()
         except discord.Forbidden:
