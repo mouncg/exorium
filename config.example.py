f7c53c0881c0c14f0d9917d42cc01714aa00f540
Bluewy <bluewyechache@gmail.com>
2021-04-05 13:31:44 +0200

    Add support var

--- config.example.py
+++ config.example.py
@@ -5,3 +5,4 @@ extensions = ['jishaku',
 
 invite = "https://discord.com/api/oauth2/authorize?client_id=620990340630970425&permissions=335932630&scope=bot"
 privacy = "https://discord.com/api/oauth2/authorize?client_id=620990340630970425&permissions=335932630&scope=bot"
+support = "https://discord.gg/CEHkNky"
