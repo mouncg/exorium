5d986a1956a581ab23cb48614cd1ba72a3fceeae
Bluewy <bluewyechache@gmail.com>
2021-04-05 13:24:22 +0200

    add privacy & invite vars

--- config.example.py
+++ config.example.py
@@ -2,3 +2,6 @@ TOKEN = ''
 
 extensions = ['jishaku',
               'cogs.help']
+
+invite = "https://discord.com/api/oauth2/authorize?client_id=620990340630970425&permissions=335932630&scope=bot"
+privacy = "https://discord.com/api/oauth2/authorize?client_id=620990340630970425&permissions=335932630&scope=bot"
