fd48c71deb6d4c1e82a6369ab5edaaba26994edc
Bluewy <bluewyechache@gmail.com>
2021-04-05 15:21:07 +0200

    add cogs.error to extensions

--- config.example.py
+++ config.example.py
@@ -1,7 +1,8 @@
 TOKEN = ''
 
 extensions = ['jishaku',
-              'cogs.help']
+              'cogs.help',
+              'cogs.error']
 
 invite = "https://discord.com/api/oauth2/authorize?client_id=620990340630970425&permissions=335932630&scope=bot"
 privacy = "https://discord.com/api/oauth2/authorize?client_id=620990340630970425&permissions=335932630&scope=bot"
