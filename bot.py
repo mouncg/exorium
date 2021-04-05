4a0f7a2ceb8fa5c5544cba5d9ac21eec126bd2cc
Bluewy <bluewyechache@gmail.com>
2021-04-05 12:23:25 +0200

    Update prefixes

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 
 def get_prefix(bot, message):
-    prefixes = ["exo ", "e!", "Exo "]
+    prefixes = ["esq ", "e!", "Esq "]
     
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
