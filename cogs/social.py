dbb14dfe1b7290dde025d5a45012176a2ffc1320
DuckMasterAl <duckmaster.dev@gmail.com>
2021-04-06 14:13:19 -0400

    Social Commands

---
+++ cogs/social.py
@@ -0,0 +1,113 @@
+import config
+import discord
+import gifs
+import random
+import aiohttp
+import json
+from discord.ext import commands
+
+from utils import default
+
+
+class social(commands.Cog, name="Social"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief="Slap someone", enabled=False)
+    async def slap(self, ctx, members: commands.Greedy[discord.Member]):
+        """Slap the specified people"""
+        if str(ctx.author.id) in str(members):
+            return await ctx.send("You can't slap yourself, derp!")
+        await functions.interactions(ctx, members, "slap", "slaps")
+
+    @commands.command(brief="Snuggle someone")
+    async def snuggle(self, ctx, members: commands.Greedy[discord.Member]):
+        """Snuggle the specified people"""
+        await default.social(ctx, members, "snuggled", gifs.snuggle, reason)
+
+    @commands.command(brief="Hug someone")
+    async def hug(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
+       await functions.interactions(ctx, members, "hugged", gifs.hug, reason, 'hug')
+
+    @commands.command(brief="Bonk someone")
+    async def bonk(self, ctx, members: commands.Greedy[discord.Member]):
+        """Bonk the specified people"""
+        await functions.interactions(ctx, members, "bonked", gifs.bonk)
+
+    @commands.command(brief="Boop someone")
+    async def boop(self, ctx, members: commands.Greedy[discord.Member]):
+        """Boop the specified people"""
+        await functions.interactions(ctx, members, "booped", gifs.boop)
+
+    @commands.command(brief="Smooch someone", aliases=["kiss"])
+    async def smooch(self, ctx, members: commands.Greedy[discord.Member]):
+        """Smooch the specified people"""
+        await functions.interactions(ctx, members, "smooched", gifs.smooch)
+
+    @commands.command(brief="Lick someone")
+    async def lick(self, ctx, members: commands.Greedy[discord.Member]):
+        """Lick the specified people"""
+        await functions.interactions(ctx, members, "licked" gifs.lick)
+
+    @commands.command(brief="Give bellyrubs!")
+    async def bellyrub(self, ctx, members: commands.Greedy[discord.Member]):
+        """Give bellyrubs to the specified people"""
+        await functions.interactions(ctx, members, "bellyrubbed", gifs.bellyrub)
+
+    @commands.command(brief="Cuddle someone")
+    async def cuddle(self, ctx, members: commands.Greedy[discord.Member]):
+        """Cuddle the specified people"""
+        await functions.interactions(ctx, members, "cuddled", gifs.cuddle)
+
+    @commands.command(brief="Feed someone")
+    async def feed(self, ctx, members: commands.Greedy[discord.Member]):
+        """Feed the specified people"""
+        await functions.interactions(ctx, members, "fed", gifs.feed)
+
+    @commands.command(brief="Glomp someone")
+    async def glomp(self, ctx, members: commands.Greedy[discord.Member]):
+        """Glomp on the specified people"""
+        await functions.interactions(ctx, members, "glomped", gifs.glomp)
+
+    @commands.command(brief="Highfive someone")
+    async def highfive(self, ctx, members: commands.Greedy[discord.Member]):
+        """Highfive the specified people"""
+        await functions.interactions(ctx, members, "highfived", gifs.highfive)
+
+    @commands.command(brief="Rawrrrr")
+    async def rawr(self, ctx, members: commands.Greedy[discord.Member]):
+        """Rawr at the specified people"""
+        await functions.interactions(ctx, members, "rawred at", gifs.rawr)
+
+    @commands.command(brief="Howl to the moon, or someone", aliases=["howl"])
+    async def awoo(self, ctx, members: commands.Greedy[discord.Member]):
+        """Howl at the specified people"""
+        await functions.interactions(ctx, members, "howled at", gifs.awoo)
+
+    @commands.command(brief="pat someone!", aliases=["pet"])
+    async def pat(self, ctx, members: commands.Greedy[discord.Member]):
+        """Pat the specified people"""
+        await functions.interactions(ctx, members, "patted", gifs.pet, 'pat')
+
+    @commands.command(brief="Gib cookie")
+    async def cookie(self, ctx, members: commands.Greedy[discord.Member]):
+        """Give cookies to the specified people"""
+        await functions.interactions(ctx, members, "gave a cookie to", gifs.cookie)
+
+    @commands.command(brief="Blushies!")
+    async def blush(self, ctx, members: commands.Greedy[discord.Member] = None):
+        """Blush (optionally because of specified people)"""
+        await functions.feelings(ctx, members, "blushes", gifs.blush)
+
+    @commands.command(brief="Be happy")
+    async def happy(self, ctx, members: commands.Greedy[discord.Member] = None):
+        """Be happy (optionally because of specified people)"""
+        await functions.feelings(ctx, members, "smiles", gifs.happy)
+
+    @commands.command(brief="wag yer tail")
+    async def wag(self, ctx, members: commands.Greedy[discord.Member] = None):
+        """Wag your tail (Optionally because of specified people)"""
+        await functions.feelings(ctx, members, "wags their tail", gifs.wag)
+
+def setup(bot):
+    bot.add_cog(social(bot))
--- utils/default.py
+++ utils/default.py
@@ -1,4 +1,45 @@
+import discord, random, aiohttp
+
 def date(target, clock=True):
     if clock is False:
         return target.strftime("%d %B %Y")
     return target.strftime("%d %B %Y, %H:%M")
+
+async def interactions(ctx, members, name, list, reason=None, sra_url=None):
+    if sra_url is None:
+        image = random.choice(list)
+    else:
+        api_random = random.choice(['normal', 'sra'])
+        if api_random == 'normal':
+            image = random.choice(list)
+        elif api_random == 'sra':
+            await ctx.trigger_typing()
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'https://some-random-api.ml/animu/{sra_url}') as r:
+                    if r.status == 200:
+                        js = await r.json()
+                        image = js['link']
+                    else:
+                        image = random.choice(list)
+    display_list = []
+    for x in members:
+        display_list.append(x.display_name)
+    if len(members) >= 3:
+        display_list.append(f"and {display_list.pop(-1)}")
+    embed = discord.Embed(description=f"**{ctx.author.display_name}** {name} **" + ', '.join(display_list) + f"**\n{'' if reason is None else f'**Reason:** {reason}'}", color=discord.Color.blue())
+    embed.set_thumbnail(url=image)
+    await ctx.send(embed=embed)
+
+async def feelings(ctx, members, name, list):
+    embed = discord.Embed(color=discord.Color.blue())
+    embed.set_thumbnail(url=image)
+    if members is None:
+        embed.description = f"**{ctx.author.display_name}** {name}!"
+    else:
+        display_list = []
+        for x in members:
+            display_list.append(x.display_name)
+        if len(members) >= 3:
+            display_list.append(f"and {display_list.pop(-1)}")
+        embed.description=f"**{ctx.author.display_name}** {name} because of {display_list}"
+    await ctx.send(embed=embed)
