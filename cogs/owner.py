efdc5d67da0e7f1da6fba4060995b9a85fe6822a
Joshua <bluewyechache@gmail.com>
2021-04-17 10:18:57 +0200

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -93,7 +93,7 @@ class owner(commands.Cog, name="Owner"):
     @commands.command()
     @commands.is_owner()
     async def leave(self, ctx, *, args):
-        guild = self.bot.get_guild(args)
+        guild = self.bot.get_guild(755068089233834074)
         await ctx.send(guild.name)
 
 
