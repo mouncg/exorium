5464cab79694d00a2470b7d1f84236be7e51b1bc
FennyNoFloofy <67643990+FennyNoFloofy@users.noreply.github.com>
2021-04-18 17:44:52 +0200

    Added nuzzle gifs

--- gifs.py
+++ gifs.py
@@ -147,3 +147,6 @@ wag = ["https://media.tenor.com/images/391f32ea35c6bd010fe29425f8a689e3/tenor.gi
        "https://media.tenor.com/images/4a53db6c0267f84b180d7b9d3c34f120/tenor.gif",
        "https://media.tenor.com/images/dd137df1f2badd31405967a12d862f28/tenor.gif",
        "https://media.tenor.com/images/7bd999202fde3cb64395725964ff34ac/tenor.gif"]
+
+
+nuzzle = ["https://tenor.com/YDnc.gif"]
