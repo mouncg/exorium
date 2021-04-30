85f9117e774e9e6e075a81471150d22db24bb601
Bluewy <bluewyechache@gmail.com>
2021-04-30 15:17:47 +0200

    Added more error handling

--- cogs/error.py
+++ cogs/error.py
@@ -1,7 +1,9 @@
 import asyncio
 import discord
 import traceback
+import config
 from discord.ext import commands
+from utils.checks import admin_only
 
 
 class error(commands.Cog, name="Error"):
@@ -9,14 +11,28 @@ class error(commands.Cog, name="Error"):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
+    async def on_command_error(self, ctx, err):
+        if isinstance(err, commands.CommandNotFound):
             pass
 
-        if isinstance(error, commands.CommandOnCooldown):
-            cd = discord.Embed(color=discord.Color.red())
-            e.description = f"This command is in cooldown."
-            return await ctx.send(embed=cd)
+        if isinstance(err, commands.MissingPermissions):
+            perms = "`" + '`, `'.join(err.missing_perms) + "`"
+            return await ctx.send(f"{config.crossmark} You are missing {perms} permissions.")
+
+        if isinstance(err, commands.BotMissingPermissions):
+            perms = "`" + '`, `'.join(err.missing_perms) + "`"
+            return await ctx.send(f"{config.crossmark} I'm missing {perms} permissions")
+
+        if isinstance(err, admin_only):
+            await ctx.send(f"{config.crossmark} You must be a bot administrator for this command.")
+
+        if isinstance(err, commands.MissingRequiredArgument):
+            return await ctx.send(f"You are missing arguments - {err.param}")
+
+        else:
+            e = discord.Embed(title="traceback", color=discord.Color.red())
+            e.description = f"**Full traceback**\n```py\n{''.join(traceback.format_exception(type(err), err, err.__traceback__))}\n```"
+            await ctx.send(embed=e)
 
 def setup(bot):
     bot.add_cog(error(bot))
