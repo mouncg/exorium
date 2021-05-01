56b29e77edb41481f042904702bfe501e9f0c6e9
Bluewy <bluewyechache@gmail.com>
2021-05-01 17:14:41 +0200

    Fetch owner, on_guild_remove

--- cogs/events/logs.py
+++ cogs/events/logs.py
@@ -15,20 +15,43 @@ class logs(commands.Cog, name="Logs"):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         log = await self.bot.fetch_channel(762203326519181312)
+        owner = await self.bot.fetch_user(guild.owner_id)
+        owner = str(owner)
 
         l = discord.Embed(color=discord.Color.green(), title="New guild joined")
         l.set_author(name=guild, icon_url=guild.icon_url)
         l.set_footer(text=f"Now in {len(self.bot.guilds)} guilds")
         l.description = f"""
-Guild **{guild}** with ID **{guild.id}**
-Owned by {guild.owner} ({guild.owner_id})
+Guild **{guild}** ({guild.id})
+Owner: **{owner}** ({guild.owner_id})
 Created on **{default.date(guild.created_at)}**
-Approximately **{len(guild.member_count)}** members
-**{guild.text_channels}** text & **{guild.voice_channels}** voice channels
+Approximately **{guild.member_count}** members
+**{len(guild.text_channels)}** text & **{len(guild.voice_channels)}** voice channels
 Icon url: **[Click here]({guild.icon_url})** 
 """
 
+        await log.send(guild.id, embed=l)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        log = await self.bot.fetch_channel(762203326519181312)
+        owner = await self.bot.fetch_user(guild.owner_id)
+        owner = str(owner)
+
+        l = discord.Embed(color=discord.Color.red(), title="Old guild left")
+        l.set_author(name=guild, icon_url=guild.icon_url)
+        l.set_footer(text=f"Now in {len(self.bot.guilds)} guilds")
+        l.description = f"""
+        Guild **{guild}** ({guild.id})
+        Owner: **{owner}** ({guild.owner_id})
+        Created on **{default.date(guild.created_at)}**
+        Approximately **{guild.member_count}** members
+        **{len(guild.text_channels)}** text & **{len(guild.voice_channels)}** voice channels
+        Icon url: **[Click here]({guild.icon_url})** 
+        """
+
         await log.send(embed=l)
+
 
 def setup(bot):
     bot.add_cog(logs(bot))
