775f309a8b06dcfe49bfa2edeafb51681f44bb13
Bluewy <bluewyechache@gmail.com>
2021-05-01 17:15:47 +0200

    Remove admin checks

--- utils/checks.py
+++ utils/checks.py
@@ -2,16 +2,6 @@ import discord
 import aiohttp
 from discord.ext import commands
 
-
-def admin():
-    async def predicate(ctx):
-        administrators = ctx.author.id == 809057677716094997 or ctx.author.id == 345457928972533773 or ctx.author.id == 443217277580738571 or ctx.author.id == 699686304388087858
-        if not administrators:
-            raise admin_only()
-        else:
-            return administrators
-    return commands.check(predicate)
-
 class BannedMember(commands.Converter):
     async def convert(self, ctx, argument):
         if argument.isdigit():
@@ -27,7 +17,3 @@ class BannedMember(commands.Converter):
             if entity is None:
                 raise commands.BadArgument('This member has not been banned before.')
             return entity
-
-
-class admin_only(commands.CheckFailure):
-    pass
