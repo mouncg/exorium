48bc9289958a8fe2476594826217c91a4e6f8987
Joshua <bluewyechache@gmail.com>
2021-05-02 15:11:34 +0200

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -15,11 +15,6 @@ class Admin(commands.Cog, name="Admin"):
     def __init__(self, bot):
         self.bot = bot
         self.help_icon = "👑"
-        
-    @commands.Cog.listener('on_guild_join')
-    async def flitz_nerd(self, guild):
-        if guild.id == 715969701771083817:
-            await guild.leave()
 
     @commands.command()
     @admin()
