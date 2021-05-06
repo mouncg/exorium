90a01c7d4ae25151c1cb6fe17b536bf8fcacf149
soulrika <45083953+soulrika@users.noreply.github.com>
2021-05-06 23:50:49 +0100

    fucky wucky

--- cogs/help.py
+++ cogs/help.py
@@ -265,7 +265,7 @@ You can get support here:
         Moksej = await self.bot.fetch_user(345457928972533773)
         Duck = await self.bot.fetch_user(443217277580738571)
         Fenny = await self.bot.fetch_user(699686304388087858)
-
+        Rika = await self.bot.fetch_user(750470053300011070)
         devs = [f"{x.name}#{x.discriminator}" for x in
                 self.bot.get_guild(755068089233834074).get_role(828339695314403378).members]
         dev1 = ', '.join(devs)
