54472fc6bef625c1f65f92d331f1c378d6657edd
Joshua <bluewyechache@gmail.com>
2021-05-14 20:02:40 +0200

    Update error.py

--- cogs/events/error.py
+++ cogs/events/error.py
@@ -119,5 +119,9 @@ class error(commands.Cog, name="Error"):
 
                 return await checkmsg.edit(embed=se)
 
+    async def bot_check(self, ctx):
+        if self.bot.lockdown:
+            return await ctx.send(f"{self.bot.user} Is currently undergoing maintenance. Please stand by and wait. If you wanna see what's going on or stay updated on the maintenance, you are free to check out [the status page](https://flitzstudios.instatus.com/)")
+
 def setup(bot):
     bot.add_cog(error(bot))
