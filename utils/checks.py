6b4a82f601bbe83f15625486df16c679cdf7b244
Joshua <bluewyechache@gmail.com>
2021-05-14 20:18:05 +0200

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -17,3 +17,11 @@ class BannedMember(commands.Converter):
             if entity is None:
                 raise commands.BadArgument('This member has not been banned before.')
             return entity
+        
+async def lockdown(ctx):
+    if ctx.bot.lockdown:
+        e = discord.Embed(color=discord.Color.red())
+        e.description = f"{self.bot.user} Is currently undergoing maintenance. Please stand by and wait. If you wanna see what's going on or stay updated on the maintenance, you are free to check out [the status page](https://flitzstudios.instatus.com/)"
+        await ctx.send(embed=e)
+        return True
+    return False
