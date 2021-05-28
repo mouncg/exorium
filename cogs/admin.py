1f77d9f2613f140de13ebd91e35224517619e3c3
Joshua <bluewyechache@gmail.com>
2021-05-28 13:32:59 +0200

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -268,7 +268,9 @@ __**Are you sure you want me to leave this guild?**__
         except Exception:
             self.bot.database.execute(f"INSERT INTO blacklist VALUES('{server}', '{reason}')")
             self.bot.blacklist[server] = reason
-            await self.bot.get_guild(server).leave()
+            guild = self.bot.get_guild(server)
+            if guild:
+                await guild.leave()
             await ctx.send(f"blacklisted {server}")
 
 
