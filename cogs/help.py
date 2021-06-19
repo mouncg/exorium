4f1145d80c4421f6ea5a00bbfb40b387582fd53c
joshua <joshuaslui0203@gmail.com>
2021-06-19 15:57:08 +0200

    remove self

--- cogs/help.py
+++ cogs/help.py
@@ -396,7 +396,7 @@ Channels:
 
     @binary.group(aliases=["d"])
     @commands.cooldown(1, 2, commands.BucketType.user)
-    async def decode(self, ctx, *, binary):
+    async def decode(ctx, *, binary):
         """ Decode binary to text """
         async with aiohttp.ClientSession() as cs:
             async with cs.get(f"https://some-random-api.ml/binary?decode={binary}") as r:
